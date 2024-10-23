20b19846e9b7473e27ed3a53baae3c4510a18a0d
tasnimalam1 <rafsantasnimrafsan@gmail.com>
2024-10-24 00:32:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-6xl font-extrabold bg-red-500'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
